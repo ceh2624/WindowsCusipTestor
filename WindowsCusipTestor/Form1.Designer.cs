69c4e71f4d4362cd55c1a5a4604155e1e861b19d
ceh2624 <ceh2624@gmail.com>
2020-08-13 15:17:51 -0400

    Add project files.

---
+++ WindowsCusipTestor/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsCusipTestor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbFirstSix = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCusip = new System.Windows.Forms.Label();
+            this.tbNextTwo = new System.Windows.Forms.TextBox();
+            this.tbSum = new System.Windows.Forms.TextBox();
+            this.lblText = new System.Windows.Forms.Label();
+            this.lblCharNums = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbFirstSix
+            // 
+            this.tbFirstSix.Location = new System.Drawing.Point(26, 21);
+            this.tbFirstSix.MaxLength = 6;
+            this.tbFirstSix.Name = "tbFirstSix";
+            this.tbFirstSix.Size = new System.Drawing.Size(63, 23);
+            this.tbFirstSix.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Validate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCusip
+            // 
+            this.lblCusip.AutoSize = true;
+            this.lblCusip.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCusip.Location = new System.Drawing.Point(26, 54);
+            this.lblCusip.Name = "lblCusip";
+            this.lblCusip.Size = new System.Drawing.Size(40, 15);
+            this.lblCusip.TabIndex = 2;
+            this.lblCusip.Text = "label1";
+            // 
+            // tbNextTwo
+            // 
+            this.tbNextTwo.Location = new System.Drawing.Point(91, 21);
+            this.tbNextTwo.MaxLength = 2;
+            this.tbNextTwo.Name = "tbNextTwo";
+            this.tbNextTwo.Size = new System.Drawing.Size(28, 23);
+            this.tbNextTwo.TabIndex = 1;
+            // 
+            // tbSum
+            // 
+            this.tbSum.Location = new System.Drawing.Point(122, 21);
+            this.tbSum.MaxLength = 1;
+            this.tbSum.Name = "tbSum";
+            this.tbSum.Size = new System.Drawing.Size(28, 23);
+            this.tbSum.TabIndex = 2;
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(32, 84);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(38, 15);
+            this.lblText.TabIndex = 4;
+            this.lblText.Text = "label2";
+            this.lblText.Visible = false;
+            // 
+            // lblCharNums
+            // 
+            this.lblCharNums.AutoSize = true;
+            this.lblCharNums.BackColor = System.Drawing.Color.Blue;
+            this.lblCharNums.ForeColor = System.Drawing.Color.Yellow;
+            this.lblCharNums.Location = new System.Drawing.Point(31, 118);
+            this.lblCharNums.Name = "lblCharNums";
+            this.lblCharNums.Size = new System.Drawing.Size(38, 15);
+            this.lblCharNums.TabIndex = 5;
+            this.lblCharNums.Text = "label1";
+            this.lblCharNums.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 178);
+            this.Controls.Add(this.lblCharNums);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.tbSum);
+            this.Controls.Add(this.tbNextTwo);
+            this.Controls.Add(this.lblCusip);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbFirstSix);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows Cusip Tester *chaswick*";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFirstSix;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCusip;
+        private System.Windows.Forms.TextBox tbNextTwo;
+        private System.Windows.Forms.TextBox tbSum;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Label lblCharNums;
+    }
+}
+
---
+++ WindowsCusipTestor/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsCusipTestor
+{
+    public partial class Form1 : Form
+    {
+        public static string validChars = ("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ*@#");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cuNum = "";
+
+            cuNum += tbFirstSix.Text + tbNextTwo.Text + tbSum.Text;
+            cuNum = cuNum.ToUpper();
+            lblCusip.Text = tbFirstSix.Text + "-" + tbNextTwo.Text + "-" + tbSum.Text;
+            lblText.Text = "The givin test number is " + tbSum.Text;
+
+            if (validate.isValid(cuNum))
+            {
+                lblCusip.Text += " is a valid CUSIP.";
+                lblCusip.ForeColor = Color.Green;
+                //lblCusip.Font.Bold = true;
+            }
+            else
+            {
+                lblCusip.Text += " is a not valid CUSIP.";
+                lblCusip.ForeColor = Color.Red;
+                //myCusip.Text += " is not a valid CUSIP.";
+            }
+            lblText.Text += " then the calculated number is " + validate.testNumber.ToString();
+            /*** for testing ***/
+            lblCharNums.Text = validate.charVal[0].ToString();
+            for(int tx = 1; tx <= 8; tx++)
+            {
+                lblCharNums.Text += " - " + validate.charVal[tx].ToString() + " - ";
+            }
+            /********************/
+        }
+    
+
+        public static int doPos(string cuChars)
+        {
+            int strPos = -1;
+
+            do
+            {
+                strPos++;
+            } while (cuChars != validChars.Substring(strPos, 1) && (strPos < validChars.Length -1));
+
+            return strPos;
+        }
+
+    }
+
+    public static class validate
+    {
+        public static int testNumber;
+        public static int[] charVal = new int[9];
+        public static bool isValid(string cusipNum)
+        {
+            int[] multiplier = new int[8];
+            multiplier[0] = 1;
+            multiplier[1] = 2;
+            multiplier[2] = 1;
+            multiplier[3] = 2;
+            multiplier[4] = 1;
+            multiplier[5] = 2;
+            multiplier[6] = 1;
+            multiplier[7] = 2;
+            
+            int checkSum = 0;
+            //int quo, rem;
+
+            if (cusipNum == "36200*AA0" || cusipNum == "31340*AA2" || cusipNum == "31358*AA1")
+                return false; //specific invalid cusips
+            else
+            {
+                if (cusipNum.Length == 9) //do the work here...
+                {
+                    for (int x = 0; x < 9; x++)
+                    {
+                        charVal[x] = Form1.doPos(cusipNum.Substring(x, 1));
+                        if (charVal[x] > Form1.validChars.Length) { return false; }
+                    }
+                    Divc dv = new Divc(); //To use my C# replacement for the C struct div_t in <stdlib.h>
+                    Divc a = new Divc();
+                    Divc b = new Divc();
+
+                    for (int y = 0; y < 8; y++)
+                    {
+                        dv.myDiv(charVal[y] * multiplier[y], 10);
+                        checkSum += dv.quot + dv.rem;
+                    }
+                    a.myDiv(checkSum, 10);
+                    b.myDiv(a.rem, 10);
+                    checkSum = 10 - b.rem;
+                    if (checkSum == 10) { checkSum = 0; }
+                    /*******FOR TESTING*******/
+                    testNumber = checkSum;
+                    if (checkSum == charVal[8])
+                        return true;
+                    else
+                        return false;
+                }
+                else
+                    return false;
+            }
+        }
+
+    }
+
+
+    public class Divc  // My C# replacement for the C struct div_t in <stdlib.h>
+    {
+        public int quot, rem;
+
+        public void myDiv(int num, int divisor)
+        {
+            quot = num / divisor;
+            rem = num % divisor;
+        }
+    }
+
+}
